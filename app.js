54baeb7d27aa767edfa539c5b9813966bb863526
Athenas García <athenasgm.proteco@gmail.com>
2025-08-16 16:11:11 -0600

    feat: implementación de funcionalidad

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+const agregarAmigo = () =>{
+    let nuevoAmigoInput = document.querySelector("#amigo");
+    let nuevoAmigo = nuevoAmigoInput.value;
+
+    if(nuevoAmigo){
+        amigos.push(nuevoAmigo);
+        console.log(amigos);
+        nuevoAmigoInput.value = "";
+        actualizarLista();
+        
+    }else{
+        alert("Por favor inserte un nombre");
+    }
+
+
+}
+
+const actualizarLista = () =>{
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    let innerHTMLLista = "";
+    for (const amigo of amigos) {
+        innerHTMLLista = `${innerHTMLLista} <li>${amigo}</li>`;
+    }
+    listaAmigos.innerHTML = innerHTMLLista;
+}
+
+const sortearAmigo = () =>{
+    if(amigos.length > 0){
+        let amigoSecretoIndice = Math.floor(Math.random() * amigos.length);
+        console.log(amigoSecretoIndice);
+        let amigoSecreto = document.getElementById("resultado");
+        amigoSecreto.innerHTML = `<li>${amigos[amigoSecretoIndice]}</li>`
+    }
+    else
+    {
+        alert("Agrega al menos un amigo");
+    }
+
+}
